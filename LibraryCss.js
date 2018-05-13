27ebe4c0fc227dafccbdfb8504e26316fb6770a1
JakeHoogakker <34159340+JakeHoogakker@users.noreply.github.com>
2018-05-13 22:47:17 +0200

    Borders and Dimensions
    The controls to the size have been changed to the arrow keys and you can now create borders, decide how big each border gets and what color

--- LibraryCss.js
+++ LibraryCss.js
@@ -1,173 +1,200 @@
 /*
 Met deze library kun je css aanpassen bij elementen op de site zelf. Dit is te gebruiken voor een cms. Het plan is om meer css onderdelen te kunnen veranderen en meer functies toe te voegen.
 */
-        
- 
-//variabelen voor de kleur en het html element
-var xyz = [0, 0, 0, 1];
-var Selement;
-var SelPos;
-var hold = false;
-
-/*JQuery*/
-
-//Haalt element op om css van aan te passen door erop te klikken
-/*$("body").on("click", function(e){
-    console.log( e.target.id ); 
-    Selement = e.target.id;
-    console.log(Selement);
-});
 
 
-
-//Verandert de height en width van het element gebaseerd op de positie van de muis
-$('body').mousemove(function(event){ 
-    //$('#' + Selement).mousemove(function(event){ 
-        $("span").text("X: " + event.pageX + ", Y: " + event.pageY); 
-        
-        $('#' + Selement).css({ "position": "relative", "top": event.pageY, "left": event.pageX, "bottom": event.pageY, "right": event.pageX });
-    //})
-    
-    $('#' + Selement).mousedown(function() {
-      $("span").text("X: " + event.pageX + ", Y: " + event.pageY); 
-            $( this ).height(event.pageY);
-        $( this ).width(event.pageX);
-    })
-   
-    //verandert de kleur van het element naar blauwer, groener of roder
-   $('#' + Selement).keypress(function(e, color){
-    console.log("anything");
-        if(e.which == 49){ 
-            xyz[0] += 5;
-        $(this).css("background-color", "rgb("+xyz[0]+","+xyz[1]+","+xyz[2]+","+xyz[3]+")");          
-        } 
-       else if(e.which == 50){ 
-            xyz[1] += 5;
-        $(this).css("background-color", "rgb("+xyz[0]+","+xyz[1]+","+xyz[2]+","+xyz[3]+")");          
-        } 
-       else if(e.which == 51){ 
-            xyz[2] += 5;
-        $(this).css("background-color", "rgb("+xyz[0]+","+xyz[1]+","+xyz[2]+","+xyz[3]+")");          
-        } 
-        else if(e.which == 52){ 
-            xyz[3] += 0.1;
-        $(this).css("background-color", "rgb("+xyz[0]+","+xyz[1]+","+xyz[2]+","+xyz[3]+")");          
-        } 
-       else if(e.which == 53){ 
-            xyz[3] -= 0.1;
-        $(this).css("background-color", "rgb("+xyz[0]+","+xyz[1]+","+xyz[2]+","+xyz[3]+")");          
-        } 
-}); 
-    
-});*/
+var Editor = (function () {
 
 
- /*document.getElementById(Selement).style.backgroundColor = "rgb("+xyz[0]+","+xyz[1]+","+xyz[2]+")";
-        
-        function extracolor(color)
-        {
-            var amount = document.getElementById("blue").value;
-            xyz[color]+= amount;
-            document.getElementById(Selement).style.backgroundColor = "rgb("+xyz[0]+","+xyz[1]+","+xyz[2]+")";
-            
-        };
+    var xyz = [0, 0, 0, 1];
+    var xyzb = [
+        [0, 0, 0, 1],
+        [0, 0, 0, 1],
+        [0, 0, 0, 1],
+        [0, 0, 0, 1]
+    ];
+    var Selement;
+    var SelPos;
+    var hold = false;
+    var height = 1;
+    var width = 1;
+    var bWidth = [1,1,1,1];
+    var bWidthF = [1,1,1,1];
+    var direction;
 
-function edit()
-{
-    $( "button" )
-  .mouseover(function() {
-    $( this ).append( "<span style='color:#f00;'>Mouse up.</span>" );
-  })
-    
-  .mousedown(function() {
-    $(this).css("color", "yellow");
-  })
- .keypress(function(e){
-    if(e.which > 48 && e.which < 58){ 
-        $( this ).height((e.which - 40) + 10);
+
+    document.body.addEventListener("click", getid);
+
+    /*if (Selement)
+        {*/
+    document.body.onmousemove = function () {
+        getposition(event)
+    };
+    document.body.onkeydown = function () {
+        keyChanges(event)
+    };
+    document.body.addEventListener("mousedown", changesize);
+
+
+
+
+
+    function getid(e) {
+
+        console.log(Selement);
+        if (e.target.id == Selement) {
+            Selement = null;
+        } else {
+            Selement = e.target.id;
+        }
     }
-    else if (e.which == 48){
-        $( this ).height(10);
+
+
+    function getposition(event) {
+        SelPos = document.getElementById(Selement);
+        SelPos.style.position = "absolute";
+        SelPos.style.left = event.pageX - 10;
+        SelPos.style.top = event.pageY - 20;
+        console.log(event.pageX + " " + event.pageY);
+
     }
-            
-});
-}*/
 
-/*Javascript*/
-
-//zit vast aan object
-
-
-document.body.addEventListener("click", getid);
-
-/*if (Selement)
-    {*/
-        document.body.onmousemove = function(){getposition(event)};
-        document.body.onkeydown = function(){changecolor(event)};
-        document.body.addEventListener("mousedown", changesize);
-    
-
-function getid(e)  
-{
-    
-    console.log(Selement);
-    if (e.target.id == Selement)
-    {
-        Selement =  null;   
-    }
-    else {
-        Selement = e.target.id;
-    }
-}
-
-
-function getposition(event)
-{
-    SelPos = document.getElementById(Selement);
-    SelPos.style.position = "absolute";
-    SelPos.style.left = event.pageX - 10;
-    SelPos.style.top = event.pageY - 20;
-    console.log(event.pageX + " " + event.pageY);
-    
-}
-
-function changesize()
-    {
+    /*function changesize() {
+        //keycodes 189 en 187
         SelPos.style.height = event.pageY;
         SelPos.style.width = event.pageX;
-        console.log("rgeidfh");
+    }*/
+
+    function keyChanges(e) {
+
+        var SelCol = document.getElementById(Selement);
+
+        if (hold == true) {
+            switch (e.which) {
+                case 49:
+                    xyzb[direction][0] += 5;
+                    break;
+                case 50:
+                    xyzb[direction][1] += 5;
+                    break;
+                case 51:
+                    xyzb[direction][2] += 5;
+                    break;
+                case 52:
+                    xyzb[direction][3] += 0.1;
+                    break;
+                case 53:
+                    xyzb[direction][3] -= 0.1;
+                    break;
+                case 55:
+                    xyzb[direction][0] -= 5;
+                    break;
+                case 56:
+                    xyzb[direction][1] -= 5;
+                    break;
+                case 57:
+                    xyzb[direction][2] -= 5;
+                    break;
+                case 38:
+                    bWidth[0]++;
+                    bWidthF[0] = bWidth[0] + "px ";
+                    direction = 0;
+                    break;
+                case 39:
+                    bWidth[1]++;
+                    bWidthF[1] = bWidth[1] + "px ";
+                    direction = 1;
+                    break;
+                case 40:
+                    bWidth[2]++;
+                    bWidthF[2] = bWidth[2] + "px ";
+                    direction = 2;
+                    break;
+                case 37:
+                    bWidth[3]++;
+                    bWidthF[3] = bWidth[3] + "px ";
+                    direction = 3;
+                    break;
+                case 87:
+                    bWidth[0]--;
+                    bWidthF[0] = bWidth[0] + "px ";
+                    direction = 0;
+                    break;
+                case 68:
+                    bWidth[1]--;
+                    bWidthF[1] = bWidth[1] + "px ";
+                    direction = 1;
+                    break;
+                case 83:
+                    bWidth[2]--;
+                    bWidthF[2] = bWidth[2] + "px ";
+                    direction = 2;
+                    break;
+                case 65:
+                    bWidth[3]--;
+                    bWidthF[3] = bWidth[3] + "px ";
+                    direction = 3;
+                    break;
+                case 189:
+                    SelCol.style.borderStyle = "solid";
+                    break;
+                case 187:
+                    SelCol.style.borderStyle = "dotted";
+                    break;
+                case 32:
+                    hold = false;
+                    break;
+            }
+        } else {
+            switch (e.which) {
+                case 49:
+                    xyz[0] += 5;
+                    break;
+                case 50:
+                    xyz[1] += 5;
+                    break;
+                case 51:
+                    xyz[2] += 5;
+                    break;
+                case 52:
+                    xyz[3] += 0.1;
+                    break;
+                case 53:
+                    xyz[3] -= 0.1;
+                    break;
+                case 55:
+                    xyz[0] -= 5;
+                    break;
+                case 56:
+                    xyz[1] -= 5;
+                    break;
+                case 57:
+                    xyz[2] -= 5;
+                    break;
+                case 38:
+                    height--;
+                    break;
+                case 40:
+                    height++;
+                    break;
+                case 37:
+                    width--;
+                    break;
+                case 39:
+                    width++;
+                    break;
+                case 32:
+                    hold = true;
+                    break;
+            }
+        }
+        SelCol.style.backgroundColor = "rgb(" + xyz[0] + "," + xyz[1] + "," + xyz[2] + "," + xyz[3] + ")";
+        SelCol.style.borderWidth = bWidthF[0] + bWidthF[1] + bWidthF[2] + bWidthF[3];
+        SelCol.style.borderColor = "rgb(" + xyzb[0][0] + ", " + xyzb[0][1] + ", " + xyzb[0][2] + ", " + xyzb[0][3] + ") rgb(" + xyzb[1][0] + ", " + xyzb[1][1] + ", " + xyzb[1][2] + ", " + xyzb[1][3] + ") rgb(" + xyzb[2][0] + ", " + xyzb[2][1] + ", " + xyzb[2][2] + ", " + xyzb[2][3] + ") rgb(" + xyzb[3][0] + ", " + xyzb[3][1] + ", " + xyzb[3][2] + ", " + xyzb[3][3] + ")";
+        console.log(bWidthF[0] + bWidthF[1]);
+        console.log(SelCol.style.border);
+        document.getElementById(Selement).style.height = height;
+        document.getElementById(Selement).style.width = width;
     }
 
-function changecolor(e){
-    
-        var SelCol = document.getElementById(Selement);
-        switch(e.which){ 
-            case 49:
-                xyz[0] += 5;
-                break;
-            case 50:
-                xyz[1] += 5;
-                break;
-            case 51:
-                xyz[2] += 5;
-                break;
-            case 52:
-                xyz[3] += 0.1;
-                break;
-            case 53:
-                xyz[3] -= 0.1;
-                break;
-            case 55:
-                xyz[0] -= 5;
-                break;
-            case 56:
-                xyz[1] -= 5;
-                break;
-            case 57:
-                xyz[2] -= 5;
-                break;
-        }   
-    SelCol.style.backgroundColor = "rgb("+xyz[0]+","+xyz[1]+","+xyz[2]+","+xyz[3]+")"; 
-}
-
-
+})();
